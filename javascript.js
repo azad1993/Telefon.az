6687d112768c88eecb311b7dc1becb764e9d7545
Azad1993 <azad.shahbazov@mail.ru>
2020-05-11 02:30:37 +0400

    new

---
+++ javascript.js
@@ -0,0 +1,43 @@
+// Dropdown menyusu üçün 
+
+function myAccFunc1() {
+    var x = document.getElementById("demoAc1");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else {
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
+  
+  function myAccFunc2() {
+    var x = document.getElementById("demoAc2");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else {
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
+
+  function myAccFunc3() {
+    var x = document.getElementById("demoAc3");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else {
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
+
+  // default üçün
+  document.getElementById("myBtn").click();
+  
+  
+  // Kənar menyunu açıb bağlamaq üçün funksiyalar
+  function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+  }
+   
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+  }
