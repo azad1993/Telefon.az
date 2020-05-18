08b61c8efcd95c6c290a678be00af39590737a6b
Azad1993 <azad.shahbazov@mail.ru>
2020-05-19 01:34:43 +0400

    Details elaveler

---
+++ Details/details.js
@@ -0,0 +1,12 @@
+
+/*Karusel üçün*/
+
+$(document).ready(function() {
+    $('.carousel').carousel()
+});
+document.documentElement.setAttribute("lang", "en");
+document.documentElement.removeAttribute("class");
+
+axe.run(function(err, results) {
+    console.log(results.violations);
+});
